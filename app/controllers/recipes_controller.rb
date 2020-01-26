f8250a4ac69064a120e47e07394d3c026eac9159
saramccombs <theSaraMcCombs@gmail.com>
2020-01-25 21:07:52 -0500

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,29 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    2.times do
+      @recipe.ingredients.build(name: nil)
+    end
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to recipe_url(@recipe)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :title,
+      ingredients_attributes: [
+        :name,
+        :quantity
+      ]
+    )
   end
 end
