854ecea061b9188e66c2c316d05cd84ac928e8c4
James Scalise <jamescscalise@gmail.com>
2020-01-31 23:14:48 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,5 +12,19 @@ class RecipesController < ApplicationController
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to recipe_path(@recipe)
+  end
+  
+  private
+  
+  def recipe_params
+    params.require(:recipe).permit(
+    :title,
+    recipes_attributes: [
+      :name,
+      :quantity
+    ]
+  )
   end
 end
